38b6fcc94a5cc30d259299cc765d65f25c42e08e
bruxo <diogo.stein@outlook.com.br>
2023-08-01 11:43:47 -0300

    Add filter

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,15 +1,23 @@
+import {useState} from 'react'
 import data from '../../data.json'
 import CarItem from '../CarItem/CarItem'
 import Filter from '../Filter/Filter'
 import styles from './CarList.module.css'
 
 function CarList() {
-  
-  const listItems = data.map(item => <CarItem key={item.id} name={item.name} description={item.description} img={item.img}/>)
+  const [filter, setFilter] = useState('')
+
+  let listItems
+
+  if(filter) {
+    listItems = data.filter(item => item.make.toLowerCase() === filter.toLowerCase()).map(item => <CarItem key={item.id} name={item.name} description={item.description} img={item.img}/>)
+  } else {
+    listItems = data.map(item => <CarItem key={item.id} name={item.name} description={item.description} img={item.img}/>)
+  }
 
   return (
     <section className='wrapper'>
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter}/>
       <ul role='list' className={styles.ul}>
       {listItems}
       </ul>
